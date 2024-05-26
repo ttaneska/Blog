0f93910b5298ea6efd0532221ff092b98498229c
Teodora <128500487+ttaneska@users.noreply.github.com>
2024-05-26 20:12:17 +0200

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+
+
+    public function run():void
+    {
+        $user=User::factory()->create([
+            'name'=>'John Doe',
+
+        ]);
+
+        Post::factory(5)->create([
+            'user_id'=>$user->id,
+        ]);
+
+
+
+    }
+}
